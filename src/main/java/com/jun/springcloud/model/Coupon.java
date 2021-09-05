5ea253f1e99249bd83659a1f5efe9af1b6697099
Junhyung Ki <kijoonh91@gmail.com>
2021-09-05 11:28:44 +0200

    coupon model and the corresponding repo created

---
+++ src/main/java/com/jun/springcloud/model/Coupon.java
@@ -0,0 +1,52 @@
+package com.jun.springcloud.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Coupon {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String code;
+	private BigDecimal discount;
+	private String exeDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(BigDecimal discount) {
+		this.discount = discount;
+	}
+
+	public String getExeDate() {
+		return exeDate;
+	}
+
+	public void setExeDate(String exeDate) {
+		this.exeDate = exeDate;
+	}
+
+}
---
+++ src/main/java/com/jun/springcloud/repos/CouponRepo.java
@@ -0,0 +1,9 @@
+package com.jun.springcloud.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jun.springcloud.model.Coupon;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+
+}
